586c81c3379b05d9af2efb8d610ff6d7b4caae5d
kobe73er <kobe73er@gmail.com>
2015-05-27 21:29:08 +0800

    add future case

--- multithreading/src/main/java/andrew/deng/multithreading/futuretasktest/CallableTestTwo.java
+++ multithreading/src/main/java/andrew/deng/multithreading/futuretasktest/CallableTestTwo.java
@@ -1,6 +1,5 @@
 package andrew.deng.multithreading.futuretasktest;
 
-import java.util.Random;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
